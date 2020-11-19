27a45982e5498be4bfdce02ed344222df6b70c64
iJinxin <874450097@qq.com>
2020-11-20 00:11:48 +0800

    计算器加权求值

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const chapter3_main = require('./chapter3/index')
 const chapter4_main = require('./chapter4/index')
+const chapter5_main = require('./chapter5/index')
 
-chapter4_main()
+chapter5_main()
