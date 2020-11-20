9682045deb24f5a2144007dc15424e7a3a909a9a
iJinxin <874450097@qq.com>
2020-11-20 18:01:38 +0800

    增加括号运算

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const chapter3_main = require('./chapter3/index')
 const chapter4_main = require('./chapter4/index')
 const chapter5_main = require('./chapter5/index')
+const chapter5_ex = require('./chapter5/exercise')
 
-chapter5_main()
+chapter5_ex()
